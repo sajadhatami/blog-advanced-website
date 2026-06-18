75004a3b50394a1b22a2029bcf029d190595803a
Sajad <sajadhatamiw@gmail.com>
2026-06-18 23:42:12 +0330

    adding shema of the models

--- blogsite/accounts/models.py
+++ blogsite/accounts/models.py
@@ -1,3 +1,26 @@
+import email
+
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionsMixin)
 
 # Create your models here.
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    #customising user
+    email = models.EmailField(unique=True,max_length=250)
+    #USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    #is he staff user or not
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is verified 
+    #is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    
+    
+    def __str__(self):
+        return self.email
--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    # a model for blogsite posts
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="post_images/", blank=True, null=True)
+    status = models.BooleanField()
+    category = models.ForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
